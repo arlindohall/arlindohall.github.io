5162e03ff69efadc5726f37bd0dba09b7ba1539a
Miller Hall <hall@hey.com>
2021-03-15 19:18:06 -0400

    Simplify compile script

--- compile.sh
+++ compile.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+# Note: you must suffix the dest path with `/` in order
+# for the pandoc macros to be set correctly (they are used
+# by the footer links)
+function pandoc-path() {
+    src=$1 ; shift
+    dest=$1 ; shift
+
+    for f in $(ls $src) ; do
+        pandoc $src/$f \
+                --template _template.html \
+                --from markdown+footnotes \
+                --to html+auto_identifiers \
+                -o ${dest}$(basename $f .md).html
+    done
+}
+
 THIS_SCRIPT=$(realpath $0)
 ROOT=$(dirname $THIS_SCRIPT)
 
@@ -11,21 +27,8 @@ if [ $ROOT != $PWD ] ; then
     MOVED=true
 fi
 
-for f in $(ls _posts) ; do
-    pandoc _posts/$f \
-            --template _template.html \
-            --from markdown+footnotes \
-            --to html+auto_identifiers \
-            -o posts/$(basename $f .md).html
-done
-
-for f in $(ls _root) ; do
-    pandoc _root/$f \
-            --template _template.html \
-            --from markdown+footnotes \
-            --to html+auto_identifiers \
-            -o $(basename $f .md).html
-done
+pandoc-path _posts posts/
+pandoc-path _root  ''
 
 if [ $MOVED ] ; then
     cd -
